7e37ab756c7cba45c34fe0eabe5c7b4e1a00e42c
Farwa Usman <farwa333.u@gmail.com>
2025-11-07 19:29:01 -0800

    DAO Creation

--- app/src/main/java/com/example/bmicalculator/BMI Record.kt
+++ app/src/main/java/com/example/bmicalculator/BMI Record.kt
@@ -1,10 +1,15 @@
 package com.example.bmicalculator
 
+import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Entity
+import androidx.room.Insert
 import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Update
 
-@Entity(tableName = "bmi record")
-data class bmiRecordEntity(
+@Entity(tableName = "bmi-record")
+data class BmiRecordEntity(
     @PrimaryKey(autoGenerate = true)
      val id: Int = 0,
     val weight:String,
@@ -14,3 +19,14 @@ data class bmiRecordEntity(
     val emoji: String,
     val time: String
 )
+@Dao
+interface BmiDao{
+    @Insert
+    suspend fun insert(bmiRecord:BmiRecordEntity)
+    @Delete
+    suspend fun delete(bmiRecord:BmiRecordEntity)
+    @Update
+    suspend fun update(bmiRecord:BmiRecordEntity)
+    @Query("SELECT*FROM `bmi-record`")
+    suspend fun getAllRecord():List<BmiRecordEntity>
+}
